693c4c9217075351d0a659067dd0b0a7d7b11a2d
Josh Williams <halfs13@gmail.com>
2014-04-09 22:10:50 -0400

    checking for ssl on profile requests

--- app/components/profile/route.js
+++ app/components/profile/route.js
@@ -32,19 +32,23 @@ module.exports = function(app, services, logger) {
 	});
 
 	app.get("/profile/?", services.profile.verifyAdmin, function(req, res) {
-		//if admin
-		services.profile.find().
-		then(function(profiles) {
-			res.json(profiles);
-			res.end();
-		}, function(err) {
-			res.json(err);
-			res.end();
-		});
+		if(req.secure) {
+			//if admin
+			services.profile.find().
+			then(function(profiles) {
+				res.json(profiles);
+				res.end();
+			}, function(err) {
+				res.json(err);
+				res.end();
+			});
+		} else {
+			services.response_handler.send403(res, "Requires SSL");
+		}
 	});
 
-	app.patch("/profile/:id([0-9a-f]+)", services.profile.verifyAdmin,
-		function(req, res) {
+	app.patch("/profile/:id([0-9a-f]+)", services.profile.verifyAdmin, function(req, res) {
+		if(req.secure) {
 			services.profile.update(req.body)
 			.then(function(result) {
 				res.json(result);
@@ -54,39 +58,45 @@ module.exports = function(app, services, logger) {
 				res.json(err);
 				res.end();
 			});
+		} else {
+			services.response_handler.send403(res, "Requires SSL");
 		}
-	);
+	});
 
 	app.post("/login/?", function(req, res) {
-		if(!req.body.email || !req.body.password) {
-			logger.error("Missing username or password");
-			res.status(403);
-			res.end();
-		} else {
-			//hash the pass
-			var hash = crypto.createHash('sha256');
-			hash.update(req.body.password, "utf8");
-			var key = hash.digest('base64');
-
-			services.profile.verify(req.body.email, key)
-			.then(function(level) {
-				logger.debug("Level: " + level);
-				if(level) {
-					console.log(level);
-					req.session.level = level;
-					req.session.cookie.expires = false;
-
-					res.json({success: true, level: level});
-					res.end();
-				} else {
-					res.status(403);
-					res.end();
-				}
-			},function(err) {
-				res.status(500);
-				res.json({error: err});
+		if(req.secure) {
+			if(!req.body.email || !req.body.password) {
+				logger.error("Missing username or password");
+				res.status(403);
 				res.end();
-			});
+			} else {
+				//hash the pass
+				var hash = crypto.createHash('sha256');
+				hash.update(req.body.password, "utf8");
+				var key = hash.digest('base64');
+
+				services.profile.verify(req.body.email, key)
+				.then(function(level) {
+					logger.debug("Level: " + level);
+					if(level) {
+						console.log(level);
+						req.session.level = level;
+						req.session.cookie.expires = false;
+
+						res.json({success: true, level: level});
+						res.end();
+					} else {
+						res.status(403);
+						res.end();
+					}
+				},function(err) {
+					res.status(500);
+					res.json({error: err});
+					res.end();
+				});
+			}
+		} else {
+			services.response_handler.send403(res, 'Requires SSL')
 		}
 	});
 
